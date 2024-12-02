3bb5543e1d930938a5e798f4b8d5c94fc98cacc2
ekamphuis82 <ekamphuis82@gmail.com>
2024-12-02 15:40:06 +0100

    fix expandable row

--- src/pages/DragNDrop.vue
+++ src/pages/DragNDrop.vue
@@ -69,7 +69,7 @@
             <q-td
               colspan="100%"
               style="border-bottom: 1px solid rgba(0, 0, 0, 0.12)"
-              >Example non draggable row
+              >Example non draggable row `{{ element.name }}`
             </q-td>
           </q-tr>
         </template>
